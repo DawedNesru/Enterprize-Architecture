cf306969080d445a5e1e220eec570bbd5a070957
unknown <dawednsr@gmail.comgit config --global user.name dawedmuzeyen>
2020-06-02 15:54:04 -0500

    later push

---
+++ cs544-master/exercises/exercise11_2/src/main/java/cs544/exercise11_2/IInventoryService.java
@@ -0,0 +1,5 @@
+package cs544.exercise11_2;
+
+public interface IInventoryService {
+	public long getNumberInStock();
+}
---
+++ cs544-master/exercises/exercise11_2/src/main/java/cs544/exercise11_2/InventoryService.java
@@ -0,0 +1,12 @@
+package cs544.exercise11_2;
+
+public class InventoryService implements IInventoryService {
+
+	
+
+	public long getNumberInStock() {
+	
+		return 78;
+	}
+
+}
